62430a9fa5a646d83611efb20c6154dfb83c8115
Icetune <109099573+Icetune@users.noreply.github.com>
2023-02-02 10:10:40 +0100

    mobile menu works

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  menuClosed = true;
+
+  toggleMenu() {
+    if (this.menuClosed) {
+      document.getElementById('menu').classList.add('menu-slide');
+      this.menuClosed = false;
+
+    } else {
+      document.getElementById('menu').classList.remove('menu-slide');
+      this.menuClosed = true;
+    }
+  }
 }
