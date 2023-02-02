c155ddcccd2ec13797b5179e1887e1ad1d787dc0
Icetune <109099573+Icetune@users.noreply.github.com>
2023-02-02 08:21:18 +0100

    skills done wth angular

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class MySkillsComponent {
 
+  skills = [
+    { name: 'Angular', img: 'assets/imgs/angular_icon.png' },
+    { name: 'Typescript', img: 'assets/imgs/typescript_icon.png' },
+    { name: 'JavaScript', img: 'assets/imgs/javascript_icon.png' },
+    { name: 'HTML', img: 'assets/imgs/html_icon.png' },
+    { name: 'Firebase', img: 'assets/imgs/firebase_icon.png' },
+    { name: 'GIT', img: 'assets/imgs/git_icon.png' },
+    { name: 'CSS', img: 'assets/imgs/css_icon.png' },
+    { name: 'REST-Api', img: 'assets/imgs/api_icon.png' },
+    { name: 'Scrum', img: 'assets/imgs/scrum_icon.png' },
+    { name: 'Material', img: 'assets/imgs/material_design_icon.png' }
+  ];
+
 }
