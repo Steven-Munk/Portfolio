35a7a5057dba099fa957307392244e0bd60a3017
Icetune <109099573+Icetune@users.noreply.github.com>
2023-02-15 16:16:12 +0100

    Portfolio Version 1.0

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -20,7 +20,7 @@ export class PortfolioComponent {
     {
       name: 'El Pollo Loco',
       foundation: 'JavaScript | HTML | CSS',
-      description: 'simple Jump-and-Run game based on an object-oriented approach. Help pepe to defeat fred the crazy chicken.',
+      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to defeat Fred, the crazy chicken.',
       img: '../assets/imgs/el_pollo_loco_img.png',
       github:'https://github.com/Icetune/Pollo-Loco',
       liveTest:'https://steven-munk.de/El%20Pollo%20Loco/index.html',
@@ -38,7 +38,7 @@ export class PortfolioComponent {
     {
       name: 'Ring of Fire',
       foundation: 'Angular | TypeScript | HTML | CSS | Firebase',
-      description: 'A drinking game where you and your friends can pull cards of a stable, each card provides a different drinking task.',
+      description: 'A drinking game where you and your friends can pull cards from a stable, each card provides a different drinking task.',
       img: '../assets/imgs/rof_img.png',
       github:'https://github.com/Icetune/Ring-of-Fire',
       liveTest:'https://ring-of-fire-ea4f3.web.app/',
@@ -47,7 +47,7 @@ export class PortfolioComponent {
     {
       name: 'Pokédex',
       foundation: 'JavaScript | HTML | CSS | Api',
-      description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      description: 'Based on the PokéAPI. A simple library that provides and catalogs pokemon information.',
       img: '../assets/imgs/pokedex_img.png',
       github:'https://github.com/Icetune/PokeDex',
       liveTest:'https://steven-munk.de/Pokedex/index.html',
